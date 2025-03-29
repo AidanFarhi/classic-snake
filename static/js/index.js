559dbfb50d37f9840b1f9df86bac8da9aedf2230
Aidan Farhi <aidanmcfarhi@gmail.com>
2025-03-28 21:20:14 -0400

    added movement

--- static/js/index.js
+++ static/js/index.js
@@ -1,33 +1,72 @@
 
-// Global constants
+//--------------- Global Constants ----------------
+const table = document.getElementById('grid-table')
 const num_rows = 30
 const num_cols = 30
 const grid = []
+const position = {
+    row: num_rows-1,
+    col: 0
+}
+//--------------------------------------------------
 
-// Generate grid
-for (let i = 0; i < num_rows; i++) {
-    const row = []
-    for (let j = 0; j < num_cols; j++) {
-        row.push(0)
+//----------------- Functions ---------------------
+function populateGrid() {
+    for (let i = 0; i < num_rows; i++) {
+        const row = []
+        for (let j = 0; j < num_cols; j++) {
+            row.push(0)
+        }
+        grid.push(row)
     }
-    grid.push(row)
 }
 
-// Set the starting position (lower left)
-grid[num_rows-1][0] = 1
-
-// Get the <table> element
-const table = document.getElementById('grid-table')
-
-// Populate the table using the grid
-grid.forEach(row => {
-    const tr = document.createElement('tr')
-    row.forEach(cell => {
-        const td = document.createElement('td')
-        if (cell == 1) {
-            td.classList.add('snake-cell')
-        }
-        tr.appendChild(td)
+function updateTable() {
+    table.innerHTML = ''
+    grid.forEach(row => {
+        const tr = document.createElement('tr')
+        row.forEach(cell => {
+            const td = document.createElement('td')
+            if (cell == 1) {
+                td.classList.add('snake-cell')
+            }
+            tr.appendChild(td)
+        })
+        table.appendChild(tr)
     })
-    table.appendChild(tr)
+}
+//--------------------------------------------------
+
+//----------------- Event Listeners ----------------
+document.addEventListener('keydown', event => {
+    if (event.key === 'w') {
+        grid[position.row][position.col] = 0
+        grid[position.row-1][position.col] = 1
+        position.row--
+        updateTable()
+    } else if (event.key === 'd') {
+        grid[position.row][position.col] = 0
+        grid[position.row][position.col+1] = 1
+        position.col++
+        updateTable()
+    } else if (event.key === 's') {
+        grid[position.row][position.col] = 0
+        grid[position.row+1][position.col] = 1
+        position.row++
+        updateTable()
+    } else if (event.key === 'a') {
+        grid[position.row][position.col] = 0
+        grid[position.row][position.col-1] = 1
+        position.col--
+        updateTable()
+    }
 })
+//--------------------------------------------------
+
+function main() {
+    populateGrid()
+    grid[position.row][position.col] = 1
+    updateTable()
+}
+
+main()
