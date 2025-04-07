b9e7ee1f1022ac442c3a4d84a6118c8e9f2a3eff
Aidan Farhi <aidanmcfarhi@gmail.com>
2025-04-06 20:33:23 -0400

    updated some stuff

--- static/js/index.js
+++ static/js/index.js
@@ -1,18 +1,23 @@
 
 //---------- Global Constants & Variables ----------
+// constants
 const table = document.getElementById('grid-table')
 const score = document.getElementById('score')
 const num_rows = 30
 const num_cols = 30
-const position = {
-    row: num_rows-1,
-    col: 0
+const new_position = {
+    headRow: num_rows-1,
+    headCol: 0,
+    tailRow: num_rows-1,
+    tailCol: 0 
 }
+
+// variables
 let grid = []
 let current_direction = 'UP'
 let current_score = 0
-let foodRow = position.row
-let foodCol = position.col
+let foodRow = new_position.headRow
+let foodCol = new_position.headCol
 let dead = false
 let invervalID = 0
 //--------------------------------------------------
@@ -25,20 +30,20 @@ function populateGrid() {
 }
 
 function placeFood() {
-    foodRow = position.row
-    foodCol = position.col
-    while (foodRow == position.row && foodCol == position.col) {
+    do {
         foodRow = Math.floor(Math.random() * num_rows)
         foodCol = Math.floor(Math.random() * num_cols)
-    }
+    } while (grid[foodRow][foodCol] != 0)
     grid[foodRow][foodCol] = 2
 }
 
-function checkIfFoodEaten() {
-    if (foodRow == position.row && foodCol == position.col) {
+function handleFoodEaten() {
+    if (foodRow == new_position.headRow && foodCol == new_position.headCol) {
         current_score++
         placeFood()
+        return true
     }
+    return false
 }
 
 function checkIfDead() {
@@ -69,29 +74,33 @@ function updateScore() {
 }
 
 function movePosition() {
-    if (current_direction == 'UP' && position.row !== 0) {
-        grid[position.row][position.col] = 0
-        grid[position.row-1][position.col] = 1
-        position.row--
-        checkIfFoodEaten()
+    if (current_direction == 'UP' && new_position.headRow !== 0) {
+        grid[new_position.headRow-1][new_position.headCol] = 1
+        new_position.headRow--
+        if (!handleFoodEaten()) {
+            grid[new_position.tailRow][new_position.tailCol] = 0
+        }
         updateTable()
-    } else if (current_direction == 'RIGHT' && position.col < num_cols - 1) {
-        grid[position.row][position.col] = 0
-        grid[position.row][position.col+1] = 1
-        position.col++
-        checkIfFoodEaten()
+    } else if (current_direction == 'RIGHT' && new_position.headCol < num_cols - 1) {
+        grid[new_position.headRow][new_position.col+1] = 1
+        new_position.headCol++
+        if (!handleFoodEaten()) {
+            grid[new_position.tailRow][new_position.tailCol] = 0
+        }
         updateTable()
-    } else if (current_direction == 'DOWN' && position.row < num_rows - 1) {
-        grid[position.row][position.col] = 0
-        grid[position.row+1][position.col] = 1
-        position.row++
-        checkIfFoodEaten()
+    } else if (current_direction == 'DOWN' && new_position.headRow < num_rows - 1) {
+        grid[new_position.headRow+1][new_position.headCol] = 1
+        new_position.headRow++
+        if (!handleFoodEaten()) {
+            grid[new_position.tailRow][new_position.tailCol] = 0
+        }
         updateTable()
-    } else if (current_direction == 'LEFT' && position.col !== 0) {
-        grid[position.row][position.col] = 0
-        grid[position.row][position.col-1] = 1
-        position.col--
-        checkIfFoodEaten()
+    } else if (current_direction == 'LEFT' && new_position.headCol !== 0) {
+        grid[new_position.headRow][new_position.headCol-1] = 1
+        new_position.headCol--
+        if (!handleFoodEaten()) {
+            grid[new_position.tailRow][new_position.tailCol] = 0
+        }
         updateTable()
     } else {
         dead = true
